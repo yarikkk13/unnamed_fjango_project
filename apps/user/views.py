0478914218b544f90dd8dba8154bc39d56e7a6a2
yarikkk13 <yaroslav.mahas@gmail.com>
2021-08-08 10:48:13 +0300

    iSort

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,8 +1,10 @@
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from .serializers import UserSerializer
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
+
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import AllowAny
+
+from .serializers import UserSerializer
 
 UserModel: User = get_user_model()
 
--- core/models/user_model.py
+++ core/models/user_model.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
 
 from core.managers import CustomUserManager
 
